59cc9c090a5747c2382560f211ad43c3262784f0
Adam Nicholls <anicholls41@gmail.com>
2024-07-08 21:35:41 +0100

    init commit - setting up structure for coursework

---
+++ Coursework_Task_3_Adam_Nicholls.java
@@ -0,0 +1,34 @@
+/**
+ * Below holds the all the code for F28LL Part 2 Task 3 Coursework. The main method is the starting point
+ * for the application to run. To meet the standards of the coursework (single .java file, all support classes
+ * have been made static - it should be noted that this isn't standard practice for a Java application and has just
+ * been done for the purpose of this coursework. A Brief explanation of what the Static keyword does in Java will
+ * be provided in my video explanation and demo of the code.
+ *
+ * @author Adam Nicholls
+ * @version 1
+ * @since 08.07.2024
+ */
+class F28LLPart2Task3 {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+    /**
+     * This class is designed to read in the CSV data file which will be processed by the
+     * functional features Java has to offer - Stream API
+     */
+    static final class CsvReader {
+
+    }
+
+    /**
+     * This class is an object representation of the data being imported by the CSV Reader.
+     * By converting the raw data into a Java object, it will allow me to perform more indepth data
+     * processing than if handled in a raw format - for example, checking individual fields for valid data,
+     * ensuring that the data object itself isn't empty or null etc...
+     */
+    static final class Data {
+
+    }
+}
